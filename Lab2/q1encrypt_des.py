63d1ef9ed7aff9163ffd23283667ef47e8fd23e6
darshanN04 <darshannakshatri@gmail.com>
2024-10-06 15:13:18 +0530

    adding lab2

--- Lab2/q1encrypt_des.py
+++ Lab2/q1encrypt_des.py
@@ -1,49 +1,28 @@
+from pydoc import plain
+
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad, unpad
-from Crypto.Random import get_random_bytes
 
-# Define the key and ensure it's 8 bytes long (64 bits for DES)
-key = b'A1B2C3D4'
-
-# DES requires an 8-byte block size for plaintext, and ECB mode doesn't use an IV
-block_size = 8
-
-# The plaintext message
 plaintext = "Confidential Data"
+block = 8
+key = b"A1B2C3D4"
 
-
-# Encrypting the message
 def encrypt_des(plaintext, key):
-    # Create a DES cipher object in ECB mode
-    cipher = DES.new(key, DES.MODE_ECB)
-
-    # Pad the plaintext to match the DES block size
-    padded_plaintext = pad(plaintext.encode(), block_size)
-
-    # Encrypt the padded plaintext
-    ciphertext = cipher.encrypt(padded_plaintext)
-
-    return ciphertext
+    cipher_key = DES.new(key, DES.MODE_ECB)
+    padding_text = pad(plaintext.encode(), block)
+    encrypted_text = cipher_key.encrypt(padding_text)
+    return encrypted_text
 
 
-# Decrypting the message
 def decrypt_des(ciphertext, key):
-    # Create a DES cipher object in ECB mode
-    cipher = DES.new(key, DES.MODE_ECB)
-
-    # Decrypt the ciphertext
-    decrypted_padded_text = cipher.decrypt(ciphertext)
-
-    # Unpad the plaintext after decryption
-    plaintext = unpad(decrypted_padded_text, block_size)
-
-    return plaintext.decode()
+    cipher_key = DES.new(key, DES.MODE_ECB)
+    decrypted_text = cipher_key.decrypt(ciphertext)
+    unpadding_text = unpad(decrypted_text, block)
+    return unpadding_text.decode()
 
 
-# Encrypt the message
-ciphertext = encrypt_des(plaintext, key)
-print("Ciphertext (hex):", ciphertext.hex())
+cipher = encrypt_des(plaintext, key)
+print("cipher text: ", cipher.hex())
+plain = decrypt_des(cipher, key)
+print("plain text: ", plain)
 
-# Decrypt the ciphertext
-decrypted_message = decrypt_des(ciphertext, key)
-print("Decrypted message:", decrypted_message)
